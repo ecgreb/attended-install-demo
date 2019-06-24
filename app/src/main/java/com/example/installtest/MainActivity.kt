55c84b182f47c30a1b4e0faca4d470002d118271
Chuck Greb <chuck@usebutton.com>
2019-06-24 15:15:53 -0400

    Adds app install button

--- app/src/main/java/com/example/installtest/MainActivity.kt
+++ app/src/main/java/com/example/installtest/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.installtest
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        install_app_button.setOnClickListener {
+            val uri = Uri.parse("market://details?id=com.boxed.prod")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage("com.android.vending")
+            startActivity(intent)
+        }
     }
 }
